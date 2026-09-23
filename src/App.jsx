f4ab432fea4474f9fff68aff1c6091bd197b30c6
desikurniasih <desskh222@gmail.com>
2026-09-23 22:43:36 +0700

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
-import { useState } from 'react'
 import Dashboard from './component/dashboard'
-import Deskripsi from './component/deskripsi'
 import './App.css'
 
 function App() {
-  const [selectedNews, setSelectedNews] = useState(null)
-
-  return selectedNews ? (
-    <Deskripsi
-      news={selectedNews}
-      onBack={() => setSelectedNews(null)}
-      onSelectNews={setSelectedNews}
-    />
-  ) : (
-    <Dashboard onSelectNews={setSelectedNews} />
-  )
+  return <Dashboard />
 }
 
 export default App
